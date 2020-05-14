3f716a75202d6608ad0d4f96c98ecd704d5e5459
Anton <antonderegt@pm.me>
2020-05-14 22:22:29 +0200

    Add loading bars

--- src/components/Venue.vue
+++ src/components/Venue.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <Loading v-if="loadingVenue" />
+  <div v-else>
     <div v-if="venue === null">No venue information...</div>
     <div v-else>
       <h3>{{ venue.name }}</h3>
@@ -9,30 +10,44 @@
       <p>Capaciteit: {{ venue.capacity }}</p>
       <p>Aanwezig: {{ venue.present }}</p>
       <p>Wachtrij:</p>
-      <p v-if="queue.length === 0">Geen wachtrij, kom snel!</p>
-      <p v-else v-for="person in queue" :key="person.id">{{ person.name }}</p>
+      <Loading v-if="loadingQueue" />
+      <div v-else>
+        <p v-if="queue.length === 0">Geen wachtrij, kom snel!</p>
+        <p v-else v-for="person in queue" :key="person.id">{{ person.name }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import Loading from "@/components/Loading.vue";
 
 export default {
   computed: mapState(["venue", "queue"]),
   data() {
     return {
-      id: ""
+      id: "",
+      loadingVenue: false,
+      loadingQueue: false
     };
   },
   methods: {
     getVenue() {
+      this.loadingVenue = true;
       this.$store
         .dispatch("bindVenue", this.id)
         .then(() => {
-          this.$store.dispatch("bindQueue", this.id).catch(error => {
-            alert("bindQueue: " + error);
-          });
+          this.loadingVenue = false;
+          this.loadingQueue = true;
+          this.$store
+            .dispatch("bindQueue", this.id)
+            .then(() => {
+              this.loadingQueue = false;
+            })
+            .catch(error => {
+              alert("bindQueue: " + error);
+            });
         })
         .catch(error => {
           alert("bindHoreca: " + error);
@@ -44,6 +59,9 @@ export default {
     //         date: Timestamp.fromDate(new Date()),
     //     })
     // }
+  },
+  components: {
+    Loading
   },
   created() {
     this.id = this.$route.params.id;
