895d73ed8bec2ea683700d88460c0aa6713845ea
Anton <antonderegt@pm.me>
2020-05-15 16:57:35 +0200

    Add onAuthStateChanged logic

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import vuetify from './plugins/vuetify';
+import vuetify from "./plugins/vuetify";
+import firebase from "firebase/app";
 
 Vue.config.productionTip = false;
+
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch("fetchUser", user);
+});
 
 new Vue({
   router,
--- src/store/index.js
+++ src/store/index.js
@@ -9,10 +9,20 @@ export default new Vuex.Store({
   state: {
     queue: [],
     venueList: [],
-    venue: {}
+    venue: {},
+    user: {
+      loggedIn: false,
+      data: null
+    }
   },
   mutations: {
-    ...vuexfireMutations
+    ...vuexfireMutations,
+    SET_LOGGED_IN(state, value) {
+      state.user.loggedIn = value;
+    },
+    SET_USER(state, data) {
+      state.user.data = data;
+    }
   },
   actions: {
     bindVenue: firestoreAction((bindFirestoreRef, id) => {
@@ -36,7 +46,24 @@ export default new Vuex.Store({
           .collection("queue")
           .orderBy("date")
       );
-    })
+    }),
+    fetchUser({ commit }, user) {
+      commit("SET_LOGGED_IN", user !== null);
+
+      if (user) {
+        commit("SET_USER", {
+          displayName: user.displayName,
+          email: user.email
+        });
+      } else {
+        commit("SET_USER", null);
+      }
+    }
+  },
+  getters: {
+    user(state) {
+      return state.user;
+    }
   },
   modules: {}
 });
