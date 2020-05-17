97054835d716887eed75a8d837c1aeb9c05a4c05
Anton <antonderegt@pm.me>
2020-05-17 19:17:18 +0200

    Fix lint issues

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-        <v-parallax
-    height="300"
-    src="https://images.unsplash.com/photo-1513151233558-d860c5398176?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-  ></v-parallax>
+    <v-parallax
+      height="300"
+      src="https://images.unsplash.com/photo-1513151233558-d860c5398176?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+    ></v-parallax>
     <v-container class="my-5">
       <v-layout row wrap justify-center class="pa-3">
         <v-flex xs12 md6>
