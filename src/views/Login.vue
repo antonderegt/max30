bfda54aba3161bb15d252078d69252853c0103ff
Anton <antonderegt@pm.me>
2020-05-27 21:21:10 +0200

    Add social signin

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,6 +5,13 @@
         <h1 class="display-1">Login</h1>
       </v-card-title>
       <v-card-text>
+        <v-btn @click="loginWithSocial('google')" class="mr-2">
+          <v-icon class="mr-2">fab fa-google</v-icon>
+          google</v-btn
+        ><v-btn @click="loginWithSocial('facebook')">
+          <v-icon class="mr-2">fab fa-facebook</v-icon>
+          facebook</v-btn
+        >
         <v-form ref="form" v-model="valid" :lazy-validation="lazy">
           <v-text-field
             @keyup.enter="login"
@@ -26,6 +33,7 @@
             required
           ></v-text-field>
         </v-form>
+        <a @click="sendPasswordResetEmail">Wachtwoord vergeten</a>
       </v-card-text>
       <v-divider></v-divider>
       <v-card-actions>
@@ -61,6 +69,43 @@ export default {
     };
   },
   methods: {
+    async loginWithSocial(provider) {
+      switch (provider) {
+        case "google":
+          provider = new firebase.auth.GoogleAuthProvider();
+          break;
+        case "facebook":
+          provider = new firebase.auth.FacebookAuthProvider();
+          break;
+      }
+      try {
+        const result = await firebase.auth().signInWithPopup(provider);
+        const hasProfile = await this.$store.dispatch(
+          "fetchProfile",
+          result.user.uid
+        );
+        if (!hasProfile) {
+          const profile = {
+            id: result.user.uid,
+            name: result.user.displayName,
+            owner: false
+          };
+          await this.$store.dispatch("createProfile", profile);
+        }
+        this.$router.push(this.$route.query.redirect || "/");
+      } catch (error) {
+        console.log("signInWithSocial: " + error);
+
+        // Handle Errors here.
+        // var errorCode = error.code;
+        // var errorMessage = error.message;
+        // // The email of the user's account used.
+        // var email = error.email;
+        // // The firebase.auth.AuthCredential type that was used.
+        // var credential = error.credential;
+        // ...
+      }
+    },
     async login() {
       if (this.$refs.form.validate()) {
         try {
@@ -71,6 +116,23 @@ export default {
           alert(err.message);
         }
         this.$router.push(this.$route.query.redirect || "/");
+      }
+    },
+    async sendPasswordResetEmail() {
+      if (!this.$refs.form.validate("email")) {
+        this.$store.dispatch("setSnackbar", {
+          show: true,
+          text: "E-mail invalid"
+        });
+      }
+      try {
+        await firebase.auth().sendPasswordResetEmail(this.email);
+        this.$store.dispatch("setSnackbar", {
+          show: true,
+          text: "Wachtwoord reset mail verzonden"
+        });
+      } catch (error) {
+        console.log("sendPasswordResetEmail: " + error);
       }
     },
     validate() {
